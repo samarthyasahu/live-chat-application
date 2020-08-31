b6b76c5bae4b020a4171a4f7cd36ac218000e64b
samarthyasahu <sahusamarthya098@gmail.com>
2020-08-31 17:51:44 +0530

    moved private info into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://SamarthyaSahu:abcd1234@live-chat.v9l7v.mongodb.net/test?retryWrites=true&w=majority', 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./model/user');
+
+
+mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true}).then(() => console.log('DB connected'))
                             .catch(err => console.log(err));
 
-                            
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) return res.json ({ success: false, err})
+        return res.status(200).json({
+            success: true
+        });
+    });
+    
+})
+
 app.get('/', (req,res) => {
-    res.send('Hello World')
-});
+    res.send("Hello World");
+})
+
 
 app.listen(5000);
