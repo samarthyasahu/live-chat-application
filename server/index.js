b27dd3eb41586c682930f32989069721585ce79c
samarthyasahu <sahusamarthya098@gmail.com>
2020-09-01 09:46:55 +0530

    heroku again

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req,res) => {
+    res.json({"Hello": "I am happy to deploy"})
+})
+
 app.get('/api/user/auth', auth, (req,res) => {
     res.status(200).json({
         _id: req.id,
